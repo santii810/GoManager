096de53ce4d087e6d8b3b5d7ccca4a1d78bbe03d
Unknown <santii810@hotmail.com>
2017-01-22 02:02:10 +0100

    change default controller name

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 *
 * Controller por defecto si no se le pasa ninguno en la URL
  */
-define("DEFAULT_CONTROLLER", "candy");
+define("DEFAULT_CONTROLLER", "status");
 
 /**
  * Default action if any action is passed in the URL
