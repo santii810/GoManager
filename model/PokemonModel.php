d461b35ac7e75128aa155dca096bf01fa84de6eb
santii810 <santii810@hotmail.com>
2017-01-24 20:26:10 +0100

    Models created

---
+++ model/PokemonModel.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Class Pokemon
+ * 
+ * Represents a Pokemon information.
+ * 
+  */
+class PokemonModel {
+
+
+  private $pokemonId;
+
+  private $pokemonName;
+
+  private $pokemonCandys;
+
+  private $pokemonEvolution;
+
+  private $pokemonPreevolution;
+
+
+
+  public function __construct($pokemonId=NULL, $pokemonName=NULL, $pokemonCandys=NULL, $pokemonEvolution=NULL, $pokemonPreevolution) {
+    $this->pokemonId = $pokemonId;
+    $this->pokemonName = $pokemonName;
+    $this->pokemonCandys = $pokemonCandys;
+    $this->pokemonEvolution = $pokemonEvolution;
+    $this->pokemonPreevolution = $pokemonPreevolution;
+
+  }
+  
+  public function getPokemonId() {
+    return $this->pokemonId;
+  }
+
+  public function getPokemonName() {
+    return $this->pokemonName;
+  }
+  
+  public function setPokemonName($pokemonName) {
+    $this->pokemonName = $pokemonName;
+  }
+
+  public function getPokemonCandys() {
+    return $this->pokemonCandys;
+  }
+
+  public function setPokemonCandys($pokemonCandys) {
+    $this->pokemonCandys = $pokemonCandys;
+  }
+
+  public function getPokemonEvolution(){
+    return $this->pokemonEvolution;
+  }
+
+  public function getPokemonPreevolution(){
+    return $this->pokemonPreevolution;
+  }
+}
---
+++ model/UserModel.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Class Pokemon
+ * 
+ * Represents a User information.
+ * 
+ */
+class UserModel {
+
+
+  private $userId;
+
+  private $userName;
+
+  private $userPass;
+
+
+  public function __construct($userId=NULL, $userName=NULL, $userPass=NULL) {
+    $this->userId = $userId;
+    $this->userName = $userName;
+    $this->userPass = $userPass;
+  }
+  
+  public function getUserId() {
+    return $this->userId;
+  }
+
+  public function setUserId($userId) {
+    $this->userId = $userId;
+  }
+
+  public function getUserName() {
+    return $this->userName;
+  }
+  
+  public function setUserName($userName) {
+    $this->userName = $userName;
+  }
+
+  public function getUserPass() {
+    return $this->userPass;
+  }
+
+  public function setUserPass($userPass) {
+    $this->userPass = $userPass;
+  }
+
+}
