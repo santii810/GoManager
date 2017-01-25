44134c6737cda967633b3bc9fafc1940c06ca126
santii810 <santii810@hotmail.com>
2017-01-25 17:59:41 +0100

    Nav bar updated

--- view/layouts/RecordView.php
+++ view/layouts/RecordView.php
@@ -5,6 +5,25 @@ require_once (__DIR__ . "/../../core/ViewManager.php");
 $view = ViewManager::getInstance ();
 $currentuser = $view->getVariable ( "currentusername" );
 require_once(__DIR__."/HeadView.php");
+?>
+<body>	
+	<nav class="fixed-nav-bar">
+
+	</nav>
+
+	<div id="record_table" class="container">
+		<div class="background_blue row">
+			<div class="background_red col-md-4">
+				p
+			</div>
+			<div class="background_green col-md-4">
+				p
+			</div>
+			<div class="background_red col-md-4">
+				p
+			</div>
+		</div>
+	</div>
 
 
-
+</body>
