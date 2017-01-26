22cdb2cd1a195aaf0d94d64c93d396b741137037
santii810 <santii810@hotmail.com>
2017-01-26 18:06:53 +0100

    Reestructure folder links fix

--- controller/RecordController.php
+++ controller/RecordController.php
@@ -1,41 +1,46 @@
 <?php
- // File: /controller/CandyController.php
-require_once(__DIR__."/../controller/BaseController.php");
 
-require_once(__DIR__."/../model/RecordModel.php");
-require_once(__DIR__."/../mapper/RecordMapper.php");
+// File: /controller/CandyController.php
+require_once(__DIR__ . "/../controller/BaseController.php");
+
+require_once(__DIR__ . "/../model/pokemon/PokemonMapper.php");
+require_once(__DIR__ . "/../model/record/RecordMapper.php");
 
 /**
-* Class CandyController
-*/
-class RecordController extends BaseController
-{
-	
-  /**
-   * Reference to the CandyMapper to interact
-   * with the database
-   * 
-   * @var CandyMapper
-   */
-  private $recordMapper;  
+ * Class CandyController
+ */
+class RecordController extends BaseController {
 
-  function __construct()
-  {
-  	parent::__construct();
+    /**
+     * Reference to the CandyMapper to interact
+     * with the database
+     * 
+     * @var CandyMapper
+     */
+    private $recordMapper;  
+    private $pokemonMapper;
 
-  	$this->recordMapper = new RecordMapper();          
 
-  }
+    function __construct() {
+        parent::__construct();
 
-  function index(){
-    // obtain the data from the database
-  	$recordList = $this->recordMapper->getAll();    
-    // put the array containing Post object to the view
-  	$this->view->setVariable("recordList", $recordList);    
+        $this->recordMapper = new RecordMapper();     
+        $this->pokemonMapper = new PokemonMapper();
 
-    // render the view (/view/posts/index.php)
-  	$this->view->render("recordList", "index");
-  }
+    }
+
+    function index() {
+        // obtain the data from the database
+        $recordList = $this->recordMapper->getAll();
+        $pokemonList = $this->pokemonMapper->getAll();
+        // put the array containing Post object to the view
+        $this->view->setVariable("recordList", $recordList); 
+        $this->view->setVariable("pokemonList", $pokemonList);
+
+        // render the view (/view/posts/index.php)
+        $this->view->render("recordList", "index");
+    }
+
 }
 
 ?>
