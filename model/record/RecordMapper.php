d693a2a0aa6fc8c946f4f6081594b7c299cbd197
santii810 <santii810@hotmail.com>
2017-01-26 18:06:34 +0100

    added constraints to table columns on recordMapper

--- model/record/RecordMapper.php
+++ model/record/RecordMapper.php
@@ -1,46 +1,56 @@
 <?php
-require_once (__DIR__ . "/../core/PDOConnection.php");
-require_once (__DIR__ . "/../model/PokemonModel.php");
+
+require_once (__DIR__ . "/../../core/PDOConnection.php");
+require_once (__DIR__ . "/../../model/record/RecordModel.php");
 /**
  * Class RecordMapper
  *
  * Database interface for Record entities
  *        
  */
+define("COL_RECORD_ID", "record_id");
+define("COL_RECORD_USER", "record_user");
+define("COL_RECORD_CANDYS", "record_candys");
+define("COL_RECORD_POKEDEX", "record_pokedex");
+define("COL_RECORD_POKEMON", "record_pokemon");
+define("COL_RECORD_DATE ", "record_date");
+
 class RecordMapper {
-	
-	/**
-	 * Reference to the PDO connection
-	 * 
-	 * @var PDO
-	 */
-	private $db;
-	public function __construct() {
-		$this->db = PDOConnection::getInstance ();
-	}
 
+    /**
+     * Reference to the PDO connection
+     * 
+     * @var PDO
+     */
+    private $db;
 
-	
-	/**
-	 * Retrieves all records
-	 *
-	 *
-	 * @throws PDOException if a database error occurs
-	 * @return Array of Record instances
-	 */
-	public function getAll() {
-		$id_user = 0;
-		$stmt = $this->db->prepare ( "SELECT * FROM record WHERE record_user=?");
-		$stmt->execute ( array (
-			$id_user
-			) );
-		$record_db = $stmt->fetchAll ( PDO::FETCH_ASSOC );
-		$recordList = array ();
+    public function __construct() {
+        $this->db = PDOConnection::getInstance();
+    }
 
-		foreach ( $record_db as $record ) {
-			array_push($recordList, new RecordModel( $record["record_id"],$record["record_user"],$record["record_candys"],$record["record_pokedex"],$record["record_pokemon"],$record["record_date"]));
-		}
-                
-		return $recordList;
-	}
+    /**
+     * Retrieves all records
+     *
+     *
+     * @throws PDOException if a database error occurs
+     * @return Array of Record instances
+     */
+    public function getAll() {
+        $id_user = 0;
+        $stmt = $this->db->prepare("SELECT * FROM record WHERE record_user=?");
+        $stmt->execute(array(
+            $id_user
+        ));
+        $db_response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $toret = array();
+
+        foreach ($db_response as $value) {
+            array_push($toret, new RecordModel($value[COL_RECORD_ID], $value[COL_RECORD_USER], 
+                    $value[COL_RECORD_CANDYS], $value[COL_RECORD_POKE], $value[COL_RECORD_POKEMON],
+                    $value[COL_RECORD_DATE]));
+        }
+
+        return $toret;
+    }
+
 }
