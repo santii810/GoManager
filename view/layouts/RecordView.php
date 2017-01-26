49d9201ea1e065b879dcc4c9a8dc83e3ea544ac0
santii810 <santii810@hotmail.com>
2017-01-26 21:25:57 +0100

    Modifi bootstram weigths to adapt to evolutive family lenght

--- view/layouts/RecordView.php
+++ view/layouts/RecordView.php
@@ -15,27 +15,53 @@ $currentuser = $view->getVariable("currentusername");
     foreach ($pokemonList as $key => $pokemon) {
         if ($pokemon->getPokemonPreevolution() == NULL) {
             $pokemonEvolutions = array();
-
-            $evolutionId = $pokemon->getPokemonEvolution();
-            do {
-                $evolution = $pokemonList[$evolutionId];
-                array_push($pokemonEvolutions, $evolution);
-                $evolutionId = $evolution->getPokemonEvolution();
-            } while ($evolution->getPokemonEvolution() != NULL);
-            printRow($pokemon, $pokemonEvolutions);
+            $evolutiveLineLength = 1;
+            if ($pokemon->getPokemonEvolution() != NULL) {
+                $evolutionId = $pokemon->getPokemonEvolution();
+                do {
+                    $evolution = $pokemonList[$evolutionId];
+                    array_push($pokemonEvolutions, $evolution);
+                    $evolutionId = $evolution->getPokemonEvolution();
+                    $evolutiveLineLength++;
+                } while ($evolution->getPokemonEvolution() != NULL);
+                printRow($pokemon, $pokemonEvolutions, $evolutiveLineLength);
+            }
         }
     }
 
-    function printRow(PokemonModel $pokemon, $evolutions) {
+    function printRow(PokemonModel $pokemon, $evolutions, $evolutiveLineLength) {
         /** show preevolution cell */
+        $firstweight = 6;
+        $restWeight = 6;
+        switch ($evolutiveLineLength) {
+            case 1:
+                $firstweight = 6;
+                $restWeight = 6;
+                break;
+            case 2:
+                $firstweight = 6;
+                $restWeight = 6;
+                break;
+            case 3:
+                $firstweight = 6;
+                $restWeight = 3;
+                break;
+            case 4:
+                $firstweight = 3;
+                $restWeight = 3;
+                break;
+
+            default:
+                break;
+        }
         echo '<div id="record-table-row" class="container"> ';
         echo '  <div class="background_blue row">';
-        echo '      <div class="background_red col-xs-4">';
+        echo '      <div class="background_red col-xs-' . $firstweight . '">';
         echo '          <img src="./assets/images/' . $pokemon->getPokemonId() . '.png" class="pokemon-image">';
         echo '      </div>';
         /* print evolutions */
         foreach ($evolutions as $evolution) {
-            echo '  <div class="background_green col-xs-4">';
+            echo '  <div class="background_green col-xs-' . $restWeight . '">';
             echo '      <img src="./assets/images/' . $evolution->getPokemonId() . '.png" class="pokemon-image">';
             echo '  </div>';
         }
