07ab670dbd6899910394d1117114eac39b367234
santii810 <santii810@hotmail.com>
2017-01-28 02:52:30 +0100

    End family evolution filter

--- view/layouts/RecordView.php
+++ view/layouts/RecordView.php
@@ -15,11 +15,18 @@ $currentuser = $view->getVariable("currentusername");
     <?php
     //tour pokemon list. Key = pokemon id
     foreach ($pokemonList as $key => $pokemon) {
-//If pokemon not have preevolution show it
-        if ($pokemon->getPokemonPreevolution() == NULL && $pokemon->isPokemonActive()) {
+//If pokemon not have preevolution or preevolution is lower than id show it alwait it is active
+
             $pokemonEvolutions = array();
             $evolutiveFamilyLength = 1;
-            if ($pokemon->getPokemonEvolution() != NULL) {
+
+            // if it dont have evolution and preevolution simply show it
+            if ($pokemon->getPokemonEvolution() != NULL || $pokemon->getPokemonPreevolution() != NULL) {
+                //in baby case put baby as fisrt pokemon os evolution family
+                if ($pokemon->getPokemonId() < $pokemon->getPokemonPreevolution()) {
+                    $idPreevolution = $pokemon->getPokemonPreevolution();
+                    $pokemon = $pokemonList[$idPreevolution];
+                }
                 $evolutionId = $pokemon->getPokemonEvolution();
                 do {
                     $evolution = $pokemonList[$evolutionId];
@@ -28,6 +35,8 @@ $currentuser = $view->getVariable("currentusername");
                     $evolutiveFamilyLength++;
                 } while ($evolution->getPokemonEvolution() != NULL);
             }
+
+            //show pokemon family
             $view->setVariable("pokemon", $pokemon);
             $view->setVariable("pokemonEvolutions", $pokemonEvolutions);
             $view->setVariable("evolutiveFamilyLength", $evolutiveFamilyLength);
