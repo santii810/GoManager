45a91fce890366c47a47bc452745307c6ad8619e
santii810 <santii810@hotmail.com>
2017-02-04 03:05:10 +0100

    Modify script to create database order and create xml file

--- AutoBackup.bat
+++ AutoBackup.bat
@@ -1,2 +1,3 @@
 cd C:\xampp\mysql\bin
-mysqldump --user=gomanager --password=gomanager gomanager > c:\xampp\htdocs\GoManager\database.sql
+mysqldump --user=gomanager --databases --password=gomanager --add-drop-database --add-drop-table gomanager > c:\xampp\htdocs\GoManager\database.sql
+mysqldump --user=gomanager --databases --password=gomanager --add-drop-database --add-drop-table --xml gomanager > c:\xampp\htdocs\GoManager\database.xml
